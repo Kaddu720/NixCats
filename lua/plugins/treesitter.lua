d9791179e6b37d5aa6172b2123724fa8802f8244
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2024-12-10 16:29:26 -0800

    resolve treesitter permissions error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,18 +5,6 @@ return {
   after = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {
-        "lua",
-        "nix",
-        "python",
-        "markdown",
-        "markdown_inline",
-        "terraform",
-        "vim",
-        "bash",
-        "json",
-        "yaml",
-      },
       highlight = { enable = true },
       indent = { enable = true },
     })

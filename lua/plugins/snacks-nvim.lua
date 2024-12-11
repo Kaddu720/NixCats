1d6bbb50e9e50457a9996b90e03db56a89781afd
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2024-12-11 14:29:31 -0800

    install slacks.nvim

--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -1,14 +1,18 @@
 return {
-  "snacks.nvim",
-  lazy = false,
-  after = function()
-    require("snacks").setup({
-      opts = {
-        lazygit = {
-          enabled = true,
-          configure = true,
-        },
-      },
-    })
-  end,
+	"snacks.nvim",
+	lazy = false,
+	after = function()
+		require("snacks").setup({
+			notifier = { enabled = true },
+			lazygit = {
+				enabled = true,
+        configure = false,
+        theme = nil
+			},
+		})
+
+		vim.keymap.set("n", "<leader>l", function()
+			Snacks.lazygit.open()
+		end, { desc = "[L]azygit" })
+	end,
 }

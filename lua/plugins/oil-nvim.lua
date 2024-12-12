a6d42f14cc65bb754c23ec1c6f246037e6a76e47
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2024-12-12 10:28:14 -0800

    oil updates

--- lua/plugins/oil-nvim.lua
+++ lua/plugins/oil-nvim.lua
@@ -1,14 +1,14 @@
 return {
-  "oil.nvim",
-  -- equivlent of VeryLazy in lazy.nvim
-  event = "DeferredUIEnter",
-  after = function()
-    require("oil").setup({
-      default_file_explorer = true,
-      view_options = {
-         show_hidden = true,
-      }
-    })
-    vim.keymap.set("n", "-", "<Cmd>Oil --float<CR>", {})
-  end,
+	"oil.nvim",
+	-- equivlent of VeryLazy in lazy.nvim
+	event = "DeferredUIEnter",
+	after = function()
+		require("oil").setup({
+			default_file_explorer = true,
+			view_options = {
+				show_hidden = true,
+			},
+		})
+		vim.keymap.set("n", "-", "<Cmd>Oil --float<CR>", {})
+	end,
 }

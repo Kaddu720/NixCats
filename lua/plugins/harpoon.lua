741cd01ec43ca932edda2e23e143d9c5910cf89c
Kaddu720 <nwilsonmalgus@gmail.com>
2024-12-12 21:07:08 -0800

    refine harpoon and telescope

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,27 +7,27 @@ return {
         save_on_toggle = true,
         save_on_change = true,
         mark_branch = true,
-      }
+      },
     })
 
     vim.keymap.set("n", "<leader>ha", function()
       require("harpoon.mark").add_file()
-    end)
+    end, { desc = "[H]arpoon [A]ppend" })
     vim.keymap.set("n", "<leader>hh", function()
       require("harpoon.ui").toggle_quick_menu()
-    end)
+    end, { desc = "[H][H]arpoon Menu Toggle" })
 
     vim.keymap.set("n", "<leader>h1", function()
       require("harpoon.ui").nav_file(1) -- navigates to file 1
-    end)
+    end, { desc = "[H]arpoon File [1]" })
     vim.keymap.set("n", "<leader>h2", function()
       require("harpoon.ui").nav_file(2) -- navigates to file 2
-    end)
+    end, { desc = "[H]arpoon File [2]" })
     vim.keymap.set("n", "<leader>h3", function()
       require("harpoon.ui").nav_file(3) -- navigates to file 3
-    end)
+    end,{ desc = "[H]arpoon File [3]" } )
     vim.keymap.set("n", "<leader>h4", function()
       require("harpoon.ui").nav_file(4) -- navigates to file 4
-    end)
+    end, { desc = "[H]arpoon File [4]" })
   end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,42 @@
 return {
-	{
-		"telescope-ui-select.nvim",
-		on_plugin = "telescope.nvim",
-	},
-	{
-		"telescope-fzf-native.nvim",
-		on_plugin = "telescope.nvim",
-	},
-	{
-		"telescope.nvim",
+  {
+    "telescope-ui-select.nvim",
+    on_plugin = "telescope.nvim",
+  },
+  {
+    "telescope-fzf-native.nvim",
+    on_plugin = "telescope.nvim",
+  },
+  {
+    "telescope.nvim",
     event = "DeferredUIEnter",
-		after = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]ile" })
-			vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "[F]ind [W]ord" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffer" })
-			vim.keymap.set("n", "<leader>fh", "<Cmd>Telescope harpoon marks<CR>", { desc = "[F]ind [H]arpoon" })
+    after = function()
+      local builtin = require("telescope.builtin")
 
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-			-- require("telescope").load_extension("fzf-native")
-		end,
-	},
+      vim.keymap.set("n", "<leader>ff", function()
+        vim.cmd("wal")
+        builtin.find_files()
+      end, { desc = "[F]ind [F]ile" })
+
+      vim.keymap.set("n", "<leader>fw", function()
+        vim.cmd("wal")
+        builtin.live_grep()
+      end, { desc = "[F]ind [W]ord" })
+
+      vim.keymap.set("n", "<leader>fb", function()
+        vim.cmd("wal")
+        builtin.buffers()
+      end, { desc = "[G]ind [B]uffers" })
+
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+      -- require("telescope").load_extension("fzf-native")
+    end,
+  },
 }

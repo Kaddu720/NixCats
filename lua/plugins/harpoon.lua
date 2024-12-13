aa81e8679cf90fac6b5ac42558bcac28fe6a7f56
Kaddu720 <nwilsonmalgus@gmail.com>
2024-12-12 18:44:59 -0800

    improve harpoon urgonmics

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,7 +13,7 @@ return {
     vim.keymap.set("n", "<leader>ha", function()
       require("harpoon.mark").add_file()
     end)
-    vim.keymap.set("n", "<leader>hm", function()
+    vim.keymap.set("n", "<leader>hh", function()
       require("harpoon.ui").toggle_quick_menu()
     end)
 

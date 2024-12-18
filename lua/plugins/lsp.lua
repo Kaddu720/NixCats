c9e43ea3a782c05e6496063154a0c26e5dbb3a08
Kaddu720 <nwilsonmalgus@gmail.com>
2024-12-18 10:17:51 -0800

    fix blink recomendations for vim settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,39 @@
 local servers = {}
 servers.lua_ls = {
-	Lua = {
-		formatters = {
-			ignoreComments = false,
-		},
-		signatureHelp = { enabled = true },
-		diagnostics = {
-			disable = { "missing-fields" },
-		},
-	},
-	telemetry = { enabled = false },
-	filetypes = { "lua" },
+  Lua = {
+    runtime = {
+      -- Tell the language server which version of Lua you're using
+      -- (most likely LuaJIT in the case of Neovim)
+      version = "LuaJIT",
+    },
+    formatters = {
+      ignoreComments = false,
+    },
+    signatureHelp = { enabled = true },
+    diagnostics = {
+      disable = { "missing-fields" },
+      -- Get the language server to recognize the `vim` global
+      globals = {
+        "vim",
+        "require",
+      },
+    },
+    workspace = {
+      -- Make the server aware of Neovim runtime files
+      library = vim.api.nvim_get_runtime_file("", true),
+    },
+  },
+  telemetry = { enabled = false },
+  filetypes = { "lua" },
 }
 
 -- Nixos
 servers.nixd = {
-	nixd = {
-		formatting = {
-			command = { "nil" },
-		},
-	},
+  nixd = {
+    formatting = {
+      command = { "nil" },
+    },
+  },
 }
 servers.nil_ls = {}
 
@@ -34,23 +48,23 @@ servers.marksman = {}
 servers.terraformls = {}
 
 return {
-	{
-		"nvim-lspconfig",
-		event = { "BufReadPost", "BufNewFile" },
-		cmd = { "LspInfo", "LspInstall", "LspUninstall" },
-		after = function()
-			for server_name, config in pairs(servers) do
-				require("lspconfig")[server_name].setup({
-          capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities),
-					settings = config,
-					filetypes = (config or {}).filetypes,
-					cmd = (config or {}).cmd,
-					root_pattern = (config or {}).root_pattern,
-				})
-			end
+  {
+    "nvim-lspconfig",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "LspInfo", "LspInstall", "LspUninstall" },
+    after = function()
+      for server_name, config in pairs(servers) do
+        require("lspconfig")[server_name].setup({
+          capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities),
+          settings = config,
+          filetypes = (config or {}).filetypes,
+          cmd = (config or {}).cmd,
+          root_pattern = (config or {}).root_pattern,
+        })
+      end
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Code" })
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Code" })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
+    end,
+  },
 }

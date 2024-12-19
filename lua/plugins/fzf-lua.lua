a63f772030bb75e0c06d6dbf4e6895593ed90d42
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2024-12-19 13:22:12 -0800

    trying out new fzf configurations

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -7,11 +7,17 @@ return {
       -- calling `setup` is optional for customization
       require("fzf-lua").setup({})
 
-      vim.keymap.set("n", "<leader>ff", function()
+      vim.keymap.set("n", "fa", function()
         vim.cmd("wal")
         require("fzf-lua").files()
-      end, { desc = "[F]ind [F]ile" })
+      end, { desc = "[F]zf [A]ppend" })
 
+      vim.keymap.set("n", "<leader>ff", function()
+        vim.cmd("wal")
+        require("fzf-lua").buffers()
+      end, { desc = "[F]z[F] buffer list" })
+      
+      -- Don't foret. If you hist C-g you can fuzzy search over your grep
       vim.keymap.set("n", "<leader>fw", function()
         vim.cmd("wal")
         require("fzf-lua").live_grep()
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,20 +13,24 @@ return {
     vim.keymap.set("n", "<leader>ha", function()
       require("harpoon.mark").add_file()
     end, { desc = "[H]arpoon [A]ppend" })
+
     vim.keymap.set("n", "<leader>hh", function()
       require("harpoon.ui").toggle_quick_menu()
-    end, { desc = "[H][H]arpoon Menu Toggle" })
+    end, { desc = "[H]arpoon Menu Toggle" })
 
-    vim.keymap.set("n", "<leader>h1", function()
+    vim.keymap.set("n", "<leader>q", function()
       require("harpoon.ui").nav_file(1) -- navigates to file 1
     end, { desc = "[H]arpoon File [1]" })
-    vim.keymap.set("n", "<leader>h2", function()
+
+    vim.keymap.set("n", "<leader>w", function()
       require("harpoon.ui").nav_file(2) -- navigates to file 2
     end, { desc = "[H]arpoon File [2]" })
-    vim.keymap.set("n", "<leader>h3", function()
+
+    vim.keymap.set("n", "<leader>e", function()
       require("harpoon.ui").nav_file(3) -- navigates to file 3
     end,{ desc = "[H]arpoon File [3]" } )
-    vim.keymap.set("n", "<leader>h4", function()
+
+    vim.keymap.set("n", "<leader>r", function()
       require("harpoon.ui").nav_file(4) -- navigates to file 4
     end, { desc = "[H]arpoon File [4]" })
   end,

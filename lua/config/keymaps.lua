72ce1fbac243eb419cbff5b09dfa125a4054ae49
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-03-24 09:41:27 -0700

    remove keymaps I didn't use

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,10 +10,5 @@ vim.keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })
 vim.keymap.set("n", "j", "gj", { noremap = true, silent = true })
 vim.keymap.set("n", "k", "gk", { noremap = true, silent = true })
 
--- Yanking from clipboard
-vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true, desc = "Yank to clipboard" })
-vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true, desc = "[p]aste from clipboard" })
-vim.keymap.set("n", "<leader>P", '"+P', { noremap = true, silent = true, desc = "[P]aste from clipboard" })
-
 -- Turn of Highlights
 vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>", { noremap = true })

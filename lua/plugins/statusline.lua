e47fbdd69937e1ce16f934ce83c1001e946e5e62
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-04-07 15:54:37 -0700

    fix theme is all modes

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -16,6 +16,14 @@ return {
     custom_auto.visual.a.fg = '#eb6f92'
     custom_auto.visual.c.bg = '#191724'
 
+    custom_auto.replace.a.bg = '#191724'
+    custom_auto.replace.a.fg = '#eb6f92'
+    custom_auto.replace.c.bg = '#191724'
+
+    custom_auto.command.a.bg = '#191724'
+    custom_auto.command.a.fg = '#eb6f92'
+    custom_auto.command.c.bg = '#191724'
+
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,

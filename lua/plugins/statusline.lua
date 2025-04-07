fed3a2df165ee0170b867af18641e3cecaa97272
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-04-07 15:33:17 -0700

    imporoved lualine theme

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -2,10 +2,24 @@ return {
 	"lualine.nvim",
 	event = "DeferredUIEnter",
 	after = function()
+    -- custom lualine theme
+    local custom_auto = require'lualine.themes.auto'
+    custom_auto.normal.a.bg = '#191724'
+    custom_auto.normal.a.fg = '#eb6f92'
+    custom_auto.normal.c.bg = '#191724'
+
+    custom_auto.insert.a.bg = '#191724'
+    custom_auto.insert.a.fg = '#eb6f92'
+    custom_auto.insert.c.bg = '#191724'
+
+    custom_auto.visual.a.bg = '#191724'
+    custom_auto.visual.a.fg = '#eb6f92'
+    custom_auto.visual.c.bg = '#191724'
+
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "auto",
+				theme = custom_auto,
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
@@ -26,9 +40,9 @@ return {
 				lualine_a = { "branch" },
 				lualine_b = {},
 				lualine_c = { "%f" },
-				lualine_x = {},
+				lualine_x = {{ "filetype", colored = false }, "encoding", "fileformat", "progress", "location"},
 				lualine_y = {},
-				lualine_z = { { "filetype", colored = false }, "encoding", "fileformat", "progress", "location" },
+				lualine_z = {  },
 			},
 			inactive_sections = {
 				lualine_a = {},

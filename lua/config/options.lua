8a3818fe806f9a2326f0d00eede69b2988be394b
Kaddu720 <nwilsonmalgus@gmail.com>
2025-04-09 00:58:06 -0700

    fix split colors

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -24,6 +24,9 @@ vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#e0def4", bg = "#191724" })
 vim.api.nvim_set_hl(0, "BlinkCmpKind", { fg = "#e0def4", bg = "#191724" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#191724", fg = "#6e6a86" })
 
+-- Splits
+vim.api.nvim_set_hl(0, "WinSeparator", { bg = "#191724", fg = "#6e6a86", bold = true })
+
 -- Line numbers
 vim.wo.number = true
 vim.wo.relativenumber = true

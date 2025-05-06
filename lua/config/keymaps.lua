e9613483289da750689f766ac2ca08cc44ee6075
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-05-06 10:58:13 -0700

    toogle virtual lines properly

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,6 +19,25 @@ vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>", { noremap = true })
 
 -- Turn off Virtual line diagnostics
 vim.keymap.set('n', 'cd', function()
-  local new_config = not vim.diagnostic.config().virtual_lines
-  vim.diagnostic.config({ virtual_lines = new_config })
-end, { desc = 'Toggle diagnostic virtual_lines' })
+  -- Get the current diagnostic configuration
+  local current_config = vim.diagnostic.config()
+
+  -- Check if virtual_lines is a table and has current_line set to true
+  -- If virtual_lines is not a table (i.e., false or nil), current_line_is_on will be false
+  local current_line_is_on = type(current_config.virtual_lines) == 'table' and
+                             current_config.virtual_lines.current_line == true
+
+  if current_line_is_on then
+    -- If current_line is ON, turn OFF all virtual lines
+    vim.diagnostic.config({
+      virtual_lines = false -- Setting to false disables all virtual lines
+    })
+  else
+    -- If current_line is OFF (or virtual_lines is false/nil), turn ON only the current line virtual line
+    vim.diagnostic.config({
+      virtual_lines = {
+        current_line = true
+      }
+    })
+  end
+end, { desc = 'Toggle diagnostic virtual_lines: none vs current_line' })

4dd9327d00d7be2ff3a26532092dd380997e9cfd
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-07-29 13:48:09 -0700

    replace hlslens with flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,59 +1,40 @@
 return {
-  {
-    "flash.nvim",
-    keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-        desc = "Flash",
-      },
-      {
-        "S",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").treesitter()
-        end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          require("flash").remote()
-        end,
-        desc = "Remote Flash",
-      },
-      {
-        "R",
-        mode = { "o", "x" },
-        function()
-          require("flash").treesitter_search()
-        end,
-        desc = "Treesitter Search",
-      },
-      {
-        "<c-s>",
-        mode = { "c" },
-        function()
-          require("flash").toggle()
-        end,
-        desc = "Toggle Flash Search",
-      },
-    },
-    after = function()
-      require("flash-nvim").setup({
-        modes = {
-          search = {
-            enabled = true,
-          },
-          char = {
-            jump_labels = true,
-          },
-        },
-      })
-    end,
-  },
+	{
+		"flash.nvim",
+		keys = {
+			{ "/", mode = { "n", "x", "o" }, desc = "Search for Text" },
+			{ "S", mode = { "n", "x", "o" }, desc = "Flash Treesitter" },
+			{ "r", mode = "o", desc = "Remote Flash" },
+			{ "R", mode = { "o", "x" }, desc = "Treesitter Search" },
+			{ "<c-s>", mode = { "c" }, desc = "Toggle Flash Search" },
+		},
+		after = function()
+			require("flash").setup({
+				modes = {
+					search = {
+						enabled = true,
+					},
+					char = {
+						jump_labels = true,
+					},
+				},
+			})
+
+			vim.keymap.set({ "n", "x", "o" }, "S", function()
+				require("flash").treesitter()
+			end, { desc = "Flash Treesitter" })
+
+			vim.keymap.set({ "o" }, "r", function()
+				require("flash").remote()
+			end, { desc = "Remote Flash" })
+
+			vim.keymap.set({ "o", "x" }, "R", function()
+				require("flash").treesitter_search()
+			end, { desc = "Treesitter Search" })
+
+			vim.keymap.set({ "c" }, "<c-s>", function()
+				require("flash").toggle()
+			end, { desc = "Toggle Flash Search" })
+		end,
+	},
 }

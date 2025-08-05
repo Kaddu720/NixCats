56b1966125449891f52008b183e2bce78b4ee27c
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-08-05 10:39:44 -0700

    update flash to load on f operator

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,6 +4,8 @@ return {
 		keys = {
 			{ "/", mode = { "n", "x", "o" }, desc = "Search for Text" },
 			{ "S", mode = { "n", "x", "o" }, desc = "Flash Treesitter" },
+			{ "f", mode = { "n", "o", "x" } },
+			{ "F", mode = { "n", "o", "x" } },
 			{ "r", mode = "o", desc = "Remote Flash" },
 			{ "R", mode = { "o", "x" }, desc = "Treesitter Search" },
 			{ "<c-s>", mode = { "c" }, desc = "Toggle Flash Search" },

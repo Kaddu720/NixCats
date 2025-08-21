f11eb9214240fc9e42911d6237e083daad788e24
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-08-21 11:11:36 -0700

    simplify colorscheme a bit

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -8,13 +8,11 @@ vim.cmd("colorscheme quiet")
 vim.cmd([[highlight Normal guibg=#191724 ctermbg=233]])
 
 -- Set highlight groups
-vim.api.nvim_set_hl(0, "Comment", { italic = true, fg = "#eb6f92" })
-
 vim.api.nvim_set_hl(0, "Normal", { fg = "#e0def4" })
 
+vim.api.nvim_set_hl(0, "Keyword", { bold = true, fg = "#e0def4" })
 vim.api.nvim_set_hl(0, "Constant", { fg = "#908caa" })
-vim.api.nvim_set_hl(0, "Keyword", { bold = true, fg = "#908caa" })
-vim.api.nvim_set_hl(0, "Operator", { fg = "#908caa" })
+vim.api.nvim_set_hl(0, "Comment", { italic = true, fg = "#eb6f92" })
 
 -- Floating windows
 vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#e0def4", bg = "#191724" })

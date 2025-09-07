0be0d353121980a304d60fda0642bad0484df95d
Kaddu720 <nwilsonmalgus@gmail.com>
2025-09-07 11:17:37 -0700

    update obsidian config to match new productivity system

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -70,12 +70,12 @@ return {
 				},
 
 				templates = {
-					folder = "resources/templates",
+					folder = "resources/templates/neovim",
 				},
 
 				daily_notes = {
 					-- Optional, if you keep daily notes in a separate directory.
-					folder = "./.",
+					folder = "~/Second_Brain/logs/daily/",
 					-- Optional, if you want to change the date format for the ID of daily notes.
 					date_format = "%Y-%m-%d",
 					-- Optional, if you want to change the date format of the default alias of daily notes.
@@ -83,7 +83,7 @@ return {
 					-- Optional, default tags to add to each new daily note created.
 					default_tags = { "daily-notes" },
 					-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-					template = "~/Second_Brain/resources/templates/daily.md",
+					template = "~/Second_Brain/resources/templates/neovim/daily.md",
 				},
 
 				-- Quick Notes

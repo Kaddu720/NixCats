d982bfb5957c9f0618be75e4c698ee7f0d58f320
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-09-12 08:51:15 -0700

    trying out bugger search in fzf

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -22,6 +22,7 @@ return {
 			-- Performance: Removed vim.cmd("wal") from all keymaps
 			vim.keymap.set("n", "<leader>fa", require("fzf-lua").files, { desc = "[F]zf [A]ppend" })
 			vim.keymap.set("n", "<leader>ff", require("fzf-lua").buffers, { desc = "[F]z[F] buffer list" })
+			vim.keymap.set("n", "<leader>fs", require("fzf-lua").treesitter, { desc = "[F]zf Symbols list" })
 			vim.keymap.set({ "n", "v" }, "<leader>ca", require("fzf-lua").lsp_code_actions, { desc = "[C]ode [A]ctions" })
 			vim.keymap.set("n", "<leader>fw", require("fzf-lua").live_grep, { desc = "[F]ind [W]ord" })
 		end,

41801c7692668563ea75f928a0e551321c438383
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-09-30 16:01:58 -0700

    fix the error of ltex opening to early in obsidian valuts

--- lua/plugins/ltex_extra.lua
+++ lua/plugins/ltex_extra.lua
@@ -14,18 +14,18 @@ return {
 						and (client.name == "ltex" or client.name == "ltex_plus")
 						and not ltex_extra_setup_done
 					then
-						local ok, ltex_extra = pcall(require, "ltex_extra")
-						if ok then
-							pcall(ltex_extra.setup, {
-								load_langs = { "en-US" },
-								init_check = true,
-								path = vim.fn.expand("~/.local/share/ltex"),
-								log_level = "none",
-							})
-							ltex_extra_setup_done = true
-						else
-							vim.notify("Failed to load ltex_extra.nvim", vim.log.levels.WARN)
-						end
+						vim.schedule(function()
+							local ok, ltex_extra = pcall(require, "ltex_extra")
+							if ok then
+								pcall(ltex_extra.setup, {
+									load_langs = { "en-US" },
+									init_check = true,
+									path = vim.fn.expand("~/.local/share/ltex"),
+									log_level = "none",
+								})
+								ltex_extra_setup_done = true
+							end
+						end)
 					end
 				end,
 			})

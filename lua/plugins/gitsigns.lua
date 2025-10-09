241ef2824e981e6947420dc19fdf7ca8b5313463
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-10-09 13:54:57 -0700

    trying to fix the issue of untracked changes

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -14,6 +14,7 @@ return {
 				delete = { text = "_" },
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
+				untracked = { text = "│" },
 			},
 			on_attach = function(bufnr)
 				local gs = package.loaded.gitsigns

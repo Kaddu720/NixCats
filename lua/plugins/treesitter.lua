d467f0c6c27bf33fbf334d00618861ff0732d13b
nwilsonAXS <133062215+nwilsonAXS@users.noreply.github.com>
2025-10-09 16:35:05 -0700

    trying some extra settings with treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,62 @@
 return {
-	"nvim-treesitter",
-	event = "DeferredUIEnter",
-	after = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = { enable = true },
-		})
-	end,
+	{
+		"nvim-treesitter",
+		event = "DeferredUIEnter",
+		after = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						node_incremental = "<CR>",
+						scope_incremental = "<S-CR>",
+						node_decremental = "<BS>",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+							["]c"] = "@class.outer",
+							["]a"] = "@parameter.inner",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[c"] = "@class.outer",
+							["[a"] = "@parameter.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter-context",
+		event = "DeferredUIEnter",
+		after = function()
+			require("treesitter-context").setup({
+				enable = true,
+				max_lines = 3,
+				trim_scope = "outer",
+			})
+		end,
+	},
 }

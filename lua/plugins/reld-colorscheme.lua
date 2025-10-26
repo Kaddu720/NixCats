e35082206218cbcd71356b49d01b16d771d4d066
Kaddu720 <nwilsonmalgus@gmail.com>
2025-10-26 15:57:43 -0700

    update color scheme again

--- lua/plugins/reld-colorscheme.lua
+++ lua/plugins/reld-colorscheme.lua
@@ -42,12 +42,12 @@ return {
 			hl("Cursor", { fg = colors.bg, bg = colors.cursor })
 			hl("Visual", { fg = colors.selection_fg, bg = colors.selection_bg })
 			
-			hl("@markup.heading.1.markdown", { fg = colors.orange, bold = true })
+			hl("@markup.heading.1.markdown", { fg = colors.light_gray, bold = true })
 			hl("@markup.heading.2.markdown", { fg = colors.blue, bold = true })
-			hl("@markup.heading.3.markdown", { fg = colors.red, bold = true })
+			hl("@markup.heading.3.markdown", { fg = colors.orange, bold = true })
 			hl("@markup.heading.4.markdown", { fg = colors.yellow, bold = true })
-			hl("@markup.heading.5.markdown", { fg = colors.purple, bold = true })
-			hl("@markup.heading.6.markdown", { fg = colors.light_gray, bold = true })
+			hl("@markup.heading.5.markdown", { fg = colors.red, bold = true })
+			hl("@markup.heading.6.markdown", { fg = colors.purple, bold = true })
 		end,
 	}
 }

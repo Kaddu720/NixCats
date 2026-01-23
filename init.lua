81ef545c01778876c2cbfb1cfbd3fdb9cc1cd328
Kaddu720 <nwilsonmalgus@gmail.com>
2026-01-23 14:16:48 -0800

    improve start up time measurement

--- init.lua
+++ init.lua
@@ -3,15 +3,20 @@ local start_time = vim.fn.reltime()
 
 -- Set up performance options early
 vim.opt.laststatus = 0 -- disable default status line
--- vim.opt.lazyredraw = true
 vim.opt.shadafile = "NONE" -- Don't read or write shada file on startup
-
 
 require("config")
 
--- Print startup time
+-- Print startup times (config load + total until UI ready)
 if vim.g.startup_time_enabled then
-	local end_time = vim.fn.reltime(start_time)
-	local startup_time = vim.fn.reltimefloat(end_time) * 1000
-	print(string.format("Neovim loaded in %.2f ms", startup_time))
+	local config_time = vim.fn.reltimefloat(vim.fn.reltime(start_time)) * 1000
+	print(string.format("Config loaded in %.2f ms", config_time))
+
+	vim.api.nvim_create_autocmd("UIEnter", {
+		once = true,
+		callback = function()
+			local total_time = vim.fn.reltimefloat(vim.fn.reltime(start_time)) * 1000
+			print(string.format("UI ready in %.2f ms (total)", total_time))
+		end,
+	})
 end

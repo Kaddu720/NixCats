747b4dac28febb3d08d4b7995a88f7c23358771c
Kaddu Wilson <kaddu.wilson@varda.com>
2026-03-05 09:36:31 -0800

    fix issues with non-ls and add security guide lines

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,8 +1,19 @@
 return {
 	"none-ls.nvim",
-	event = "LspAttach",
+	event = { "BufReadPre", "BufNewFile" },
 	after = function()
 		local null_ls = require("null-ls")
+		local builtins = null_ls.builtins
+
+		local function has_exec(cmd)
+			return vim.fn.executable(cmd) == 1
+		end
+
+		local function add_if_exec(sources, cmd, source)
+			if has_exec(cmd) then
+				table.insert(sources, source)
+			end
+		end
 
 		local function find_vale_root(bufname)
 			if not bufname or bufname == "" then
@@ -16,27 +27,35 @@ return {
 			return vim.fn.fnamemodify(found, ":h")
 		end
 
-		local sources = {
-			-- lua
-			null_ls.builtins.formatting.stylua,
-			null_ls.builtins.diagnostics.selene.with({
+		local sources = {}
+
+		-- lua
+		add_if_exec(sources, "stylua", builtins.formatting.stylua)
+		add_if_exec(
+			sources,
+			"selene",
+			builtins.diagnostics.selene.with({
 				condition = function(utils)
 					return utils.root_has_file({ "selene.toml" })
 				end,
-			}),
+			})
+		)
 
-			-- python
-			null_ls.builtins.diagnostics.mypy,
+		-- python
+		add_if_exec(sources, "mypy", builtins.diagnostics.mypy)
 
-			-- nix
-			null_ls.builtins.formatting.alejandra,
-			null_ls.builtins.diagnostics.deadnix,
+		-- nix
+		add_if_exec(sources, "alejandra", builtins.formatting.alejandra)
+		add_if_exec(sources, "deadnix", builtins.diagnostics.deadnix)
 
-			-- yaml
-			null_ls.builtins.diagnostics.yamllint,
+		-- yaml
+		add_if_exec(sources, "yamllint", builtins.diagnostics.yamllint)
 
-			-- markdown
-			null_ls.builtins.diagnostics.vale.with({
+		-- markdown
+		add_if_exec(
+			sources,
+			"vale",
+			builtins.diagnostics.vale.with({
 				filetypes = { "markdown" },
 				runtime_condition = function(params)
 					return find_vale_root(params.bufname) ~= nil
@@ -44,12 +63,12 @@ return {
 				cwd = function(params)
 					return find_vale_root(params.bufname)
 				end,
-			}),
+			})
+		)
 
-			-- bash
-			null_ls.builtins.formatting.shfmt,
-			null_ls.builtins.diagnostics.dotenv_linter,
-		}
+		-- bash
+		add_if_exec(sources, "shfmt", builtins.formatting.shfmt)
+		add_if_exec(sources, "dotenv-linter", builtins.diagnostics.dotenv_linter)
 
 		null_ls.setup({ sources = sources })
 	end,

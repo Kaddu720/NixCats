c4c7c9881a34804f8fdb4e879f46d4a85e37f79f
Kaddu Wilson <kaddu.wilson@varda.com>
2026-07-20 09:20:36 -0700

    fix color scheme

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,7 +1,18 @@
 vim.opt.termguicolors = true
 vim.cmd.colorscheme("quiet")
 
-local colors = { bg = "#191724", fg = "#e0def4", gray = "#908caa", border = "#6e6a86", red = "#b4637a" }
+local colors = {
+	bg = "#191724",
+	fg = "#e0def4",
+	gray = "#908caa",
+	border = "#6e6a86",
+	red = "#b4637a",
+	orange = "#bb5c3a",
+	yellow = "#c78645",
+	blue = "#3b83aa",
+	purple = "#563ea9",
+	light_gray = "#c7c4c4",
+}
 local function hl(group, opts)
 	vim.api.nvim_set_hl(0, group, opts)
 end
@@ -24,3 +35,11 @@ hl("Statement", { fg = colors.fg, bold = true })
 hl("PreProc", { fg = colors.fg })
 hl("Special", { fg = colors.fg })
 hl("Operator", { fg = colors.fg })
+
+-- Markdown headings from the previous Reld theme.
+hl("@markup.heading.1.markdown", { fg = colors.light_gray, bold = true })
+hl("@markup.heading.2.markdown", { fg = colors.blue, bold = true })
+hl("@markup.heading.3.markdown", { fg = colors.orange, bold = true })
+hl("@markup.heading.4.markdown", { fg = colors.yellow, bold = true })
+hl("@markup.heading.5.markdown", { fg = colors.red, bold = true })
+hl("@markup.heading.6.markdown", { fg = colors.purple, bold = true })
